952de11c7f8a0d819121b68d6ad1da8aed56883d
Mladen Paic <paic.m@live.com>
2021-04-05 18:29:51 +0200

    composer autoload

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '0d634767cdb37813f93eada29c2121c0c5ea1606',
+    'name' => 'mlapai/star-fleet-commander',
+  ),
+  'versions' => 
+  array (
+    'mlapai/star-fleet-commander' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '0d634767cdb37813f93eada29c2121c0c5ea1606',
+    ),
+  ),
+);
